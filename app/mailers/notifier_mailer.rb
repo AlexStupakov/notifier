7f6f67425a17580f281b48a0ef5253082244dd78
Oleksandr Stupakov <stepan.alex@gmail.com>
2020-06-15 02:47:29 +0300

    create customized emails for every Message

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -2,10 +2,22 @@ class NotifierMailer < ApplicationMailer
   default from: "notifier@example.com"
   USER_FIELDS_TO_TEMPLATE = %w[first_name last_name email phone]
 
+  # create customized emails for every Message
+  # if a message name is 'information', creates #information_message
+  # then you can use NotifierMailer.information_message.deliver_now for message with name 'information'
+  Message.all.each do |message|
+    define_method("#{message.name.underscore}_message") do |user|
+      shared_email(user, message)
+    end
+  end
+
   def shared_email(user, message)
     @user = user
     @message_body = fill_template(message.template, USER_FIELDS_TO_TEMPLATE)
-    mail(to: @user.email, subject: message.name)
+    puts "------------------------------------------------"
+    puts @message_body
+    puts "------------------------------------------------"
+    mail(to: @user.email, subject: message.name, template_name: 'shared_email')
   end
 
   private
