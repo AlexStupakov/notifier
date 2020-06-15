d98362148234870388e40a2664432d6e7e3b5a50
Oleksandr Stupakov <stepan.alex@gmail.com>
2020-06-15 05:40:31 +0300

    removed unused files

--- test/mailers/previews/notifier_mailer_preview.rb
+++
@@ -1,6 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/notifier_mailer
-class NotifierMailerPreview < ActionMailer::Preview
-  def shared_email_preview
-    NotifierMailer.shared_email(User.first, Message.first)
-  end
-end
